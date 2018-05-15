97610939118fb86baa6a3ca7b88dc67c1dad9a48
lufetoace <38989645+lufetoace@users.noreply.github.com>
2018-05-15 15:21:55 -0500

    Add files via upload

---
+++ THIGSPEAK.ino
@@ -0,0 +1,9 @@
+/*
+  *****************************************************************************************
+  **** Visit https://www.thingspeak.com to sign up for a free account and create
+  **** a channel.  The video tutorial http://community.thingspeak.com/tutorials/thingspeak-channels/
+  **** has more information. You need to change this to your channel, and your write API key
+  **** IF YOU SHARE YOUR CODE WITH OTHERS, MAKE SURE YOU REMOVE YOUR WRITE API KEY!!
+  *****************************************************************************************/
+unsigned long myChannelNumber = ide del canal;
+const char * myWriteAPIKey = "apikey del canal";
